b2c69132f2871ca526c896b12d7a3319d4685f5b
M-Saeb <Mohammed.saeb98@hotmail.com>
2022-07-08 14:13:19 +0300

    Enhancment: started working on creating tests

---
+++ test/test_db.py
@@ -0,0 +1,28 @@
+from db import DB
+import pytest
+
+class TestDB:
+	def test_db_connection(self):
+		demo_db_path = "demo.db"
+		test_db = DB(demo_db_path)
+
+	def test_creating_model(self):
+		pass
+
+	def test_reading_model(self):
+		pass
+
+	def test_updateing_model(self):
+		pass
+
+	def test_deleting_model(self):
+		pass
+
+	def test_creating_record(self):
+		pass
+
+	def test_updating_record(self):
+		pass
+
+	def test_deleting_record(self):
+		pass
